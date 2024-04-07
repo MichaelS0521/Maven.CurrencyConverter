caaada99a3caf6cce3efb479a1e1c4eb4bac6769
MichaelS0521 <michael@Zipcoders-MacBook-Pro-4.local>
2024-04-07 16:16:27 -0400

    Finished 300 Tests Passes

--- src/main/java/io/zipcoder/currencyconverterapplication/currencies/SingaporeDollar.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/currencies/SingaporeDollar.java
@@ -1,6 +1,22 @@
 package io.zipcoder.currencyconverterapplication.currencies;
 
 import io.zipcoder.currencyconverterapplication.ConvertableCurrency;
+import io.zipcoder.currencyconverterapplication.CurrencyType;
 
 public class SingaporeDollar implements ConvertableCurrency {
+    public CurrencyType currencyType;
+
+        public SingaporeDollar() {
+        currencyType = CurrencyType.SINGAPORE_DOLLAR;
+    }
+
+    @Override
+    public Double convert(CurrencyType currencyType) {
+        return currencyType.getRate() / this.currencyType.getRate();
+    }
+
+    @Override
+    public CurrencyType getTypeOfCurrency() {
+        return this.currencyType;
+    }
 }
